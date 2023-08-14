3a5a874abf541b27a51ba2cfc80f3fc256a2ebc8
Diwakar Sharma <diwakarchiku12@gmail.com>
2023-08-14 16:53:27 +0530

    3rd Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String arg[]) {
+        RWOnly obj = new RWOnly();
+
+//        obj.name = "Diwakar";
+//        System.out.prntln(obj.name);
+
+        obj.setName("Diwakar Sharma");
+        System.out.println(obj.getName());
+    }
 }
